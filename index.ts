115efbd8b553927b2279cee7ab2436ab35960695
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-22 21:56:42 +0200

    Added image links to properties

--- index.ts
+++ index.ts
@@ -65,7 +65,7 @@ const properties : {
     contact: string;
     isAvailable: boolean;
 }[]= [{
-    image: '',
+    image: 'https://www.google.com/imgres?q=polish%20cottage&imgurl=https%3A%2F%2Fi.redd.it%2Fs7oic08mh8y41.jpg&imgrefurl=https%3A%2F%2Fwww.reddit.com%2Fr%2FFairytaleasFuck%2Fcomments%2Fgi0txd%2Ftraditional_style_painted_polish_cottage%2F&docid=vZhv5ogiaE0zEM&tbnid=0lyBQQnssQ3q_M&vet=12ahUKEwjXy6T32_CJAxUpWUEAHW3uNPoQM3oECHsQAA..i&w=960&h=720&hcb=2&ved=2ahUKEwjXy6T32_CJAxUpWUEAHW3uNPoQM3oECHsQAA',
     title: 'Polish Cottage',
     price: 34,
     location: {
@@ -78,7 +78,7 @@ const properties : {
     isAvailable: false,
 },
 {
-    image: '',
+    image: 'https://www.google.com/imgres?q=columbian%20shack&imgurl=https%3A%2F%2Ftheworldonmynecklace.com%2Fwp-content%2Fuploads%2F2023%2F04%2FHumpback-Turtle-bungalow.jpg&imgrefurl=https%3A%2F%2Ftheworldonmynecklace.com%2Fel-valle-colombia-pacific-coast%2F&docid=lGwvOMP3YysaiM&tbnid=2qi3cnB8ZId4IM&vet=12ahUKEwir74SC3PCJAxXkZ0EAHcOIM2sQM3oECE8QAA..i&w=1100&h=733&hcb=2&ved=2ahUKEwir74SC3PCJAxXkZ0EAHcOIM2sQM3oECE8QAA',
     title: 'Colombian Shack',
     price: 100,
     location: {
@@ -91,7 +91,7 @@ const properties : {
     isAvailable: true,
 },
 {
-    image: '',
+    image: 'https://www.google.com/imgres?q=california%20hotel&imgurl=https%3A%2F%2Fdynamic-media-cdn.tripadvisor.com%2Fmedia%2Fphoto-o%2F29%2F69%2Fe2%2Fe1%2Fckh-ss-hotelproperty.jpg%3Fw%3D700%26h%3D-1%26s%3D1&imgrefurl=https%3A%2F%2Fwww.tripadvisor.com%2FHotel_Review-g45963-d91763-Reviews-California_Hotel_and_Casino-Las_Vegas_Nevada.html&docid=yOjeRfieUP6_zM&tbnid=OK7MiBviSEGmhM&vet=12ahUKEwi8naaN3PCJAxX2QkEAHfLOAd0QM3oECB0QAA..i&w=700&h=700&hcb=2&ved=2ahUKEwi8naaN3PCJAxX2QkEAHfLOAd0QM3oECB0QAA',
     title: 'California Hotel',
     price: 234,
     location: {
