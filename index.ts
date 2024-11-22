1cc4c089c93f865de2a5935ac62d1583a067e6b0
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-22 17:16:29 +0200

    Populating user name on welcome functionality

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@
 // all TypeScript weakness flags.
 // : number
 const reviewTotalDisplay = document.querySelector('#reviews')
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
 
 const reviews = [
     {
@@ -31,3 +33,18 @@ function showReviewTotal( value : number, lastReviewer : string, isLoyaltyMember
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+const you = {
+    userName: {firstName: 'Bobby', lastName: 'Brown'},
+    isReturning: true,
+}
+
+function populateUser( isReturning : boolean , userName : string ){
+    if (isReturning){
+        reviewTotalDisplay.innerHTML = 'back'
+    } else {
+        userNameDisplay.innerHTML = userName
+    }
+}
+
+populateUser( you.isReturning , you.userName.firstName +  ' ' + you.userName.lastName )
